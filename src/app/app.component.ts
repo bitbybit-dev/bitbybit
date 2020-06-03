7bcb283275f487ffad653263f12d39a938a074ac
Matas <matas@raptech.io>
2020-06-03 20:52:36 +0200

    update some typescript standards and set up fix for canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { svgResize, inject, Theme, Xml, WorkspaceSvg } from 'blockly';
 import * as JavaScript from 'blockly/javascript';
 
-import "@babylonjs/core/Meshes/meshBuilder";
+import '@babylonjs/core/Meshes/meshBuilder';
 // Side-effects only imports allowing the standard material to be used as default.
-import "@babylonjs/core/Materials/standardMaterial";
+import '@babylonjs/core/Materials/standardMaterial';
 // Side-effects only imports allowing Mesh to create default shapes (to enhance tree shaking, the construction methods on mesh are not available if the meshbuilder has not been imported).
-import { Engine } from "@babylonjs/core/Engines/engine";
-import { Scene } from "@babylonjs/core/scene";
-import { Vector3, Color4, Color3 } from "@babylonjs/core/Maths/math";
-import { Mesh } from "@babylonjs/core/Meshes/mesh";
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { Scene } from '@babylonjs/core/scene';
+import { Vector3, Color4, Color3 } from '@babylonjs/core/Maths/math';
+import { Mesh } from '@babylonjs/core/Meshes/mesh';
 import { themeStyle } from './models/theme-styles.model';
 import { toolboxDefinition } from './models/toolbox-definition';
 import { ArcRotateCamera, DirectionalLight } from '@babylonjs/core';
@@ -40,77 +40,80 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-        var theme = Theme.defineTheme('themeName', themeStyle());
+        setTimeout(() => {
+            const theme = Theme.defineTheme('themeName', themeStyle());
 
-        this.blocklyArea = document.getElementById('blocklyArea');
-        this.blocklyDiv = document.getElementById('blocklyDiv');
-        this.workspace = inject(this.blocklyDiv,
-            {
-                toolbox: document.getElementById('toolbox'),
-                zoom:
+            this.blocklyArea = document.getElementById('blocklyArea');
+            this.blocklyDiv = document.getElementById('blocklyDiv');
+            this.workspace = inject(this.blocklyDiv,
                 {
-                    controls: true,
-                    wheel: true,
-                    startScale: 0.7,
-                    maxScale: 3,
-                    minScale: 0.3,
-                    scaleSpeed: 1.2
-                },
-                trashcan: false,
-                theme
+                    toolbox: document.getElementById('toolbox'),
+                    zoom:
+                    {
+                        controls: true,
+                        wheel: true,
+                        startScale: 0.7,
+                        maxScale: 3,
+                        minScale: 0.3,
+                        scaleSpeed: 1.2
+                    },
+                    trashcan: false,
+                    theme
+                });
+
+            window.addEventListener('resize', () => this.onResize(), false);
+
+            this.onResize();
+
+            svgResize(this.workspace);
+
+            const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+            this.engine = new Engine(canvas);
+            this.scene = new Scene(this.engine);
+            this.scene.clearColor = new Color4(1, 1, 1, 1);
+
+            const camera = new ArcRotateCamera('Camera', 0, 10, 10, new Vector3(0, 0, 0), this.scene);
+            camera.setPosition(new Vector3(0, 10, 20));
+            camera.attachControl(canvas, true);
+            const light = new DirectionalLight('DirectionalLight', new Vector3(1, 1, 0), this.scene);
+            light.diffuse = new Color3(1, 1, 1);
+            light.specular = new Color3(1, 1, 1);
+            light.intensity = 1;
+            this.windowBlockly = {};
+            this.windowBlockly.scene = this.scene;
+            (window as any).blockly = this.windowBlockly;
+
+            this.engine.runRenderLoop(() => {
+                this.scene.render();
             });
 
-        window.addEventListener('resize', () => this.onResize(), false);
+            this.route.queryParamMap.subscribe(param => {
+                const exampleParam = param.get('examples');
+                if (exampleParam === 'advanced-lines-between-two-surfaces') {
+                    const xml = Xml.textToDom(advancedLinesBetweenTwoSurfaces());
+                    this.workspace.clear();
+                    Xml.domToWorkspace(xml, this.workspace);
+                    this.workspace.zoomToFit();
+                    this.workspace.zoomCenter(-3);
+                    this.run();
+                }
+            });
+        }, 500);
 
-        this.onResize();
-
-        svgResize(this.workspace);
-
-        const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
-        this.engine = new Engine(canvas);
-        this.scene = new Scene(this.engine);
-        this.scene.clearColor = new Color4(1, 1, 1, 1);
-
-        var camera = new ArcRotateCamera("Camera", 0, 10, 10, new Vector3(0, 0, 0), this.scene);
-        camera.setPosition(new Vector3(0, 10, 20));
-        camera.attachControl(canvas, true);
-        var light = new DirectionalLight("DirectionalLight", new Vector3(1, 1, 0), this.scene);
-        light.diffuse = new Color3(1, 1, 1);
-        light.specular = new Color3(1, 1, 1);
-        light.intensity = 1;
-        this.windowBlockly = {};
-        this.windowBlockly.scene = this.scene;
-        (window as any).blockly = this.windowBlockly;
-
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
-        });
-
-        this.route.queryParamMap.subscribe(param => {
-            const exampleParam = param.get('examples');
-            if (exampleParam === 'advanced-lines-between-two-surfaces') {
-                var xml = Xml.textToDom(advancedLinesBetweenTwoSurfaces());
-                this.workspace.clear();
-                Xml.domToWorkspace(xml, this.workspace);
-                this.workspace.zoomToFit();
-                this.workspace.zoomCenter(-3);
-                this.run();
-            }
-        });
     }
 
     ngOnInit(): void {
-        var el = document.getElementById('blocklyArea');
-        el.insertAdjacentHTML('afterend', toolboxDefinition())
+        const el = document.getElementById('blocklyArea');
+        el.insertAdjacentHTML('afterend', toolboxDefinition());
     }
 
     onResize() {
-        var element = this.blocklyArea;
+        let element = this.blocklyArea;
         if (this.engine) {
             this.engine.resize();
         }
-        var x = 0;
-        var y = 0;
+        let x = 0;
+        let y = 0;
         do {
             x += element.offsetLeft;
             y += element.offsetTop;
@@ -119,36 +122,36 @@ export class AppComponent implements OnInit, AfterViewInit {
         this.blocklyDiv.style.width = this.blocklyArea.offsetWidth + 'px';
         this.blocklyDiv.style.height = this.blocklyArea.offsetHeight + 'px';
         svgResize(this.workspace);
-    };
+    }
 
     onCode(code: string) { }
 
     import() {
-        var inputFileElement = document.getElementById("exampleInput") as HTMLInputElement;
+        const inputFileElement = document.getElementById('exampleInput') as HTMLInputElement;
         inputFileElement.click();
         inputFileElement.onchange = (e) => {
-            var file = inputFileElement.files[0];
+            const file = inputFileElement.files[0];
             if (file) {
-                var reader = new FileReader();
-                reader.readAsText(file, "UTF-8");
+                const reader = new FileReader();
+                reader.readAsText(file, 'UTF-8');
                 reader.onload = (evt) => {
-                    var xml = Xml.textToDom(evt.target.result as string);
+                    const xml = Xml.textToDom(evt.target.result as string);
                     this.workspace.clear();
                     Xml.domToWorkspace(xml, this.workspace);
                     this.workspace.zoomToFit();
                     this.workspace.zoomCenter(-3);
                     this.run();
-                }
+                };
                 reader.onerror = (evt) => {
-                    document.getElementById("fileContents").innerHTML = "error reading file";
-                }
+                    document.getElementById('fileContents').innerHTML = 'error reading file';
+                };
             }
         };
     }
 
     export() {
-        var xml = Xml.workspaceToDom(this.workspace);
-        var xml_text = Xml.domToText(xml);
+        const xml = Xml.workspaceToDom(this.workspace);
+        let xml_text = Xml.domToText(xml);
         const blob = new Blob([xml_text], { type: 'text/xml' });
         const blobUrl = URL.createObjectURL(blob);
 
@@ -172,7 +175,7 @@ export class AppComponent implements OnInit, AfterViewInit {
         this.openSponsorsDialog();
     }
 
-    cleanCanvas(){
+    cleanCanvas() {
         this.workspace.clear();
         this.router.navigate(['/']);
         this.run();
@@ -183,11 +186,11 @@ export class AppComponent implements OnInit, AfterViewInit {
             this.scene.meshes.forEach(m => m.dispose());
             this.scene.meshes = [];
             this.scene.clearColor = new Color4(1, 1, 1, 1);
-            
+
             const javascript = JavaScript;
             (window as any).LoopTrap = 10000;
             javascript.INFINITE_LOOP_TRAP = 'if(--window.LoopTrap == 0) throw "Infinite loop cancelled after 10000 iterations.";\n';
-            let code = javascript.workspaceToCode(this.workspace);
+            const code = javascript.workspaceToCode(this.workspace);
             eval(`
 'use reserved'
 let scene = window.blockly.scene;
@@ -200,18 +203,18 @@ ${code}
                 title: 'Code execution failed',
                 details: `Something went wrong when running the code. Check if there are no disconnected or misconfigured components on your canvas`,
                 message: `Error Message: "${e}"`,
-            })
+            });
         }
     }
 
     runCodeWithDateFunction() {
-        let code = JavaScript.workspaceToCode(this.workspace);
+        const code = JavaScript.workspaceToCode(this.workspace);
         return Function(code)()();
     }
 
     evaluate = () => {
         try {
-            let code = JavaScript.workspaceToCode(this.workspace);
+            const code = JavaScript.workspaceToCode(this.workspace);
             eval(code);
         } catch (e) {
             alert(e);
