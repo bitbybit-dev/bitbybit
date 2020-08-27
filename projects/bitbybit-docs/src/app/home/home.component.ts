d0bbdd26b3b3243bce195105c1b9c77d189fe67b
Matas Ubarevicius <matasubarevicius@Matass-MacBook-Pro.local>
2020-08-27 13:23:50 +0300

    setup homepage for home.bitbybit.dev

--- projects/bitbybit-docs/src/app/home/home.component.ts
+++ projects/bitbybit-docs/src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+    loaded = false;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    getStarted() {
+        window.open('https://bitbybit.dev');
+    }
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.loaded = true;
+        }, 1000);
+    }
 
 }
