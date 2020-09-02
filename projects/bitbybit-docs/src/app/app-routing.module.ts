c8cf98f0ec5e683004c3dd5c0600b5cf6fd1a852
Matas Ubarevicius <matasubarevicius@Matass-MacBook-Pro.local>
2020-09-02 15:23:12 +0300

    introduction to UI tutorial

--- projects/bitbybit-docs/src/app/app-routing.module.ts
+++ projects/bitbybit-docs/src/app/app-routing.module.ts
@@ -3,12 +3,37 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DocumentationComponent } from './documentation/documentation.component';
 import { TutorialsComponent } from './tutorials/tutorials.component';
+import { IntroductionToUiComponent } from './tutorials/introduction-to-ui/introduction-to-ui.component';
+import { TutorialsHomeComponent } from './tutorials/tutorials-home/tutorials-home.component';
+import { ExplanationOfAlgorithmsComponent } from './tutorials/explanation-of-algorithms/explanation-of-algorithms.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'documentation', component: DocumentationComponent },
-    { path: 'tutorials', component: TutorialsComponent },
-];
+    {
+        path: 'tutorials',
+        component: TutorialsComponent,
+        children: [
+            {
+                path: '',
+                component: TutorialsHomeComponent
+            },
+            {
+                path: 'getting-started',
+                children: [
+                    {
+                        path: 'introduction-to-ui',
+                        component: IntroductionToUiComponent
+                    },
+                    {
+                        path: 'explanation-of-algorithms',
+                        component: ExplanationOfAlgorithmsComponent
+                    }
+                ]
+            },
+        ]
+
+    }];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- projects/bitbybit-docs/src/app/tutorials/tutorials.component.ts
+++ projects/bitbybit-docs/src/app/tutorials/tutorials.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 
 @Component({
-  selector: 'app-tutorials',
-  templateUrl: './tutorials.component.html',
-  styleUrls: ['./tutorials.component.scss']
+    selector: 'app-tutorials',
+    templateUrl: './tutorials.component.html',
+    styleUrls: ['./tutorials.component.scss']
 })
 export class TutorialsComponent implements OnInit {
 
-  constructor() { }
+    sideNavOpen = true;
+    treeControl = new NestedTreeControl<TutorialNode>(node => node.children);
+    dataSource = new MatTreeNestedDataSource<TutorialNode>();
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.dataSource.data = TREE_DATA;
+    }
 
+    hasChild = (_: number, node: TutorialNode) => !!node.children && node.children.length > 0;
+    ngOnInit() {
+    }
+
+    toggleSidenav() {
+        this.sideNavOpen = !this.sideNavOpen;
+    }
 }
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface TutorialNode {
+    name: string;
+    link?: string;
+    children?: TutorialNode[];
+}
+
+const TREE_DATA: TutorialNode[] = [
+    {
+        name: 'Tutorials',
+        link: '/tutorials',
+    },
+    {
+        name: 'Getting Started',
+        children: [
+            { name: 'Introduction to UI', link: 'getting-started/introduction-to-ui' },
+        ]
+    }
+];
