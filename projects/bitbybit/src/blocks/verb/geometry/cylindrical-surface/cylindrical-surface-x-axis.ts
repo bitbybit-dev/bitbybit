9573961fbee0592b381a930b974c60701e99013e
Matas Ubarevicius <matas@bitbybit.dev>
2021-01-06 16:55:03 +0200

    fixed small issue with method name

--- projects/bitbybit/src/blocks/verb/geometry/cylindrical-surface/cylindrical-surface-x-axis.ts
+++ projects/bitbybit/src/blocks/verb/geometry/cylindrical-surface/cylindrical-surface-x-axis.ts
@@ -35,7 +35,7 @@ export function createCylindricalSurfaceXAxisBlock(): void {
         (block as any).validationModel = runtimeValidationModel;
 
         const code = createStandardContextIIFE(block, blockSelector, inputs, true,
-            `return bitbybit.surface.cylindrical.xaxis(inputs);`
+            `return bitbybit.surface.cylindrical.xAxis(inputs);`
         );
         return [code, (JavaScript as any).ORDER_ATOMIC];
     };
