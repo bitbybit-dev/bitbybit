c76c5d85b749ccf38ad8d8a42471ea2fd59b36b5
Matas Ubarevicius <matas@bitbybit.dev>
2022-03-22 12:41:30 +0200

    code for bitbybit-core npm package, no angular.

--- csg.js
+++
@@ -1,4 +0,0 @@
-window.CSG = require('./node_modules/@jscad/modeling/src/index.js');
-window.CSG.IOUTILS = require('./node_modules/@jscad/io-utils/index.js');
-window.CSG.STLSERIALIZER = require('./node_modules/@jscad/stl-serializer/index.js');
-
