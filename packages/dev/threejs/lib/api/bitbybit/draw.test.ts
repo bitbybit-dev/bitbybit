b58e29f01902182aa6ea4346f816981977ae4c38
Matas Ubarevicius <matas@bitbybit.dev>
2024-11-04 20:53:50 +0200

    fixed drawing of verb curves and additional unit tests

--- packages/dev/threejs/lib/api/bitbybit/draw.test.ts
+++ packages/dev/threejs/lib/api/bitbybit/draw.test.ts
@@ -9,6 +9,7 @@ import * as Inputs from "../inputs";
 
 describe("Draw unit tests", () => {
     let draw: Draw;
+    let tag: Tag;
 
     beforeAll(async () => {
         const context = new Context();
@@ -21,7 +22,7 @@ describe("Draw unit tests", () => {
         const vector = new Vector(context, math, geometryHelper);
         const drawHelper = new DrawHelper(context, solidText, vector, jscadWorkerManager, occtWorkerManager);
         context.scene = new Scene();
-        const tag = new Tag(context);
+        tag = new Tag(context);
         draw = new Draw(drawHelper, context, tag);
     });
 
@@ -304,6 +305,21 @@ describe("Draw unit tests", () => {
             expect(ptMesh.geometry.attributes.position.array.toString()).toEqual("1,-3,3,0,-3,4");
         });
 
+        it("should draw lines via draw any with options", () => {
+            const options = {
+                ...new Inputs.Draw.DrawBasicGeometryOptions(),
+                size: 4,
+                colours: "#ff0000",
+            };
+            const res = draw.drawAny({ entity: [{ start: [1, -3, 3], end: [0, -3, 4] }, { start: [1, 3, 3], end: [0, 3, -4] }], options });
+
+            expect(res.userData.type).toBe(Inputs.Draw.drawingTypes.lines);
+
+            const ptMesh = res.children[0] as LineSegments;
+            expect(res.name).toContain("polylines");
+            expect(ptMesh.geometry.attributes.position.array.toString()).toEqual("1,-3,3,0,-3,4,1,3,3,0,3,-4");
+        });
+
         it("should update a line via draw any with options", () => {
             const options = {
                 ...new Inputs.Draw.DrawBasicGeometryOptions(),
@@ -319,6 +335,21 @@ describe("Draw unit tests", () => {
 
             const lineSegments2 = res2.children[0] as LineSegments;
             expect(lineSegments2.geometry.attributes.position.array.toString()).toEqual("1,3,-33,0,-33,4");
+        });
+
+        it("should create a polyline via draw any with options", () => {
+            const options = {
+                ...new Inputs.Draw.DrawBasicGeometryOptions(),
+                size: 4,
+                colours: "#ff00ff",
+                updatable: false,
+            };
+            const res = draw.drawAny({ entity: { points: [[1, -3, 3], [0, -3, 4], [3, 4, 5]] }, options });
+
+            expect(res.userData.type).toBe(Inputs.Draw.drawingTypes.polyline);
+
+            const lineSegments1 = res.children[0] as LineSegments;
+            expect(lineSegments1.geometry.attributes.position.array.toString()).toEqual("1,-3,3,0,-3,4,0,-3,4,3,4,5");
         });
 
         it("should update a polyline via draw any with options", () => {
@@ -354,7 +385,7 @@ describe("Draw unit tests", () => {
 
             expect(res2.userData.type).toBe(Inputs.Draw.drawingTypes.polylines);
             expect(res.name).not.toEqual(res2.name);
-            
+
             const lineSegments1 = res.children[0] as LineSegments;
             const lineSegments2 = res2.children[0] as LineSegments;
 
@@ -363,4 +394,57 @@ describe("Draw unit tests", () => {
             expect(lineSegments2.geometry.attributes.position.array.toString()).toEqual("2,-4,5,1,-2,3,1,-2,3,4,6,7,4,6,7,3,4,6,9,-4,2,3,-3,5,3,-3,5,6,4,3");
         });
     });
+
+    describe("Draw verb geometry tests", () => {
+        it("should draw a curve", async () => {
+            const curveMock = {
+                tessellate: () => {
+                    return [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]];
+                },
+                _data: { controlPoints: [], knots: 3, degree: 3 },
+            };
+            const options = {
+                ...new Inputs.Draw.DrawBasicGeometryOptions(),
+                size: 4,
+                colours: "#ff0000",
+                updatable: true,
+            };
+            const res = await draw.drawAnyAsync({ entity: curveMock, options });
+            expect(res.userData.type).toBe(Inputs.Draw.drawingTypes.verbCurve);
+            expect(res).toBeDefined();
+            expect(res.name).toContain("polyline");
+            expect(res.children.length).toBe(1);
+            const lineSegments = res.children[0] as LineSegments;
+            expect(lineSegments.geometry.attributes.position.array.toString()).toEqual("1,2,3,2,3,4,2,3,4,3,4,5,3,4,5,4,5,6");
+        });
+
+        it("should draw curves", async () => {
+            const curveMock1 = {
+                tessellate: () => {
+                    return [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]];
+                },
+                _data: { controlPoints: [], knots: 3, degree: 3 },
+            };
+            const curveMock2 = {
+                tessellate: () => {
+                    return [[3, 2, 3], [4, 3, 4], [3, 5, 5], [3, 5, 6]];
+                },
+                _data: { controlPoints: [], knots: 3, degree: 3 },
+            };
+            const options = {
+                ...new Inputs.Draw.DrawBasicGeometryOptions(),
+                size: 4,
+                colours: "#ff0000",
+                updatable: true,
+            };
+            const res = await draw.drawAnyAsync({ entity: [curveMock1, curveMock2], options });
+            expect(res.userData.type).toBe(Inputs.Draw.drawingTypes.verbCurves);
+            expect(res).toBeDefined();
+            expect(res.name).toContain("polyline");
+            const lineSegments = res.children[0] as LineSegments;
+            expect(lineSegments.geometry.attributes.position.array.toString()).toEqual("1,2,3,2,3,4,2,3,4,3,4,5,3,4,5,4,5,6,3,2,3,4,3,4,4,3,4,3,5,5,3,5,5,3,5,6");
+        });
+    });
+
+
 });
--- packages/dev/threejs/lib/api/draw-helper.ts
+++ packages/dev/threejs/lib/api/draw-helper.ts
@@ -284,7 +284,7 @@ export class DrawHelper extends DrawHelperCore {
     }
 
     drawCurves(inputs: Inputs.Verb.DrawCurvesDto<Group>): Group {
-        const points = inputs.curves.map(s => s.tessellate());
+        const points = inputs.curves.map(s => ({points: s.tessellate()}));
         return this.drawPolylinesWithColours({ polylines: points, ...inputs });
     }
 
