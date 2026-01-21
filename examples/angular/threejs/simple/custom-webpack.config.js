9a6c45f031816afe6e94090fefdda1d92d6184b5
Matas Ubarevicius <matas@bitbybit.dev>
2026-01-21 16:24:46 +0200

    migrated angular laptop holder example to new angular, removed simple threejs example as it was based on old angular and did not provide value anymore.

---
+++ examples/angular/threejs/simple/custom-webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.wasm$/,
+                type: "javascript/auto",
+                loader: "file-loader",
+            },
+            {
+                test: /\.m?js/,
+                type: "javascript/auto",
+            },
+            {
+                test: /\.m?js/,
+                resolve: {
+                    fullySpecified: false,
+                },
+            }
+        ]
+    },
+    resolve: {
+        fallback: {
+            fs: false,
+            path: false,
+            crypto: false,
+            module: false
+        }
+    }
+};
