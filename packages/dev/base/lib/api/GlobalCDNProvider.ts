5e8e52f2b67c93c6c2d79fd85e16dd8825e143b8
Matas Ubarevicius <matas@bitbybit.dev>
2026-05-08 11:21:32 +0300

    v1.0.0

--- packages/dev/base/lib/api/GlobalCDNProvider.ts
+++ packages/dev/base/lib/api/GlobalCDNProvider.ts
@@ -1,3 +1,3 @@
 export class GlobalCDNProvider {
-    static BITBYBIT_CDN_URL = "https://git-cdn.bitbybit.dev/v1.0.0-rc.2";
+    static BITBYBIT_CDN_URL = "https://git-cdn.bitbybit.dev/v1.0.0";
 }
--- packages/dev/create-app/src/index.ts
+++ packages/dev/create-app/src/index.ts
@@ -842,7 +842,7 @@ async function main(): Promise<void> {
     program
         .name("@bitbybit-dev/create-app")
         .description("Scaffold a new Bit By Bit Developers 3D/CAD project")
-        .version("1.0.0-rc.2")
+        .version("1.0.0")
         .argument("[project-name]", "Name of the project to create")
         .option("-e, --engine <engine>", "Game engine to use (threejs, babylonjs, playcanvas)")
         .option("-o, --occt-architecture <arch>", "OCCT worker architecture (32, 64, 64-mt). Default: 32")
