54288bbb22a93307b325c545c6d87fe31c0f05fd
Matas Ubarevicius <matas@bitbybit.dev>
2026-05-19 14:58:50 +0300

    double quotes

--- examples/react/babylonjs/laptop-holder/src/occ.worker.ts
+++ examples/react/babylonjs/laptop-holder/src/occ.worker.ts
@@ -1,13 +1,13 @@
 /// <reference lib="webworker" />
 /*eslint no-restricted-globals: 0*/
-import createBitbybitOcct from '@bitbybit-dev/occt/bitbybit-dev-occt/cdn';
-import { type BitbybitOcctModule } from '@bitbybit-dev/occt/bitbybit-dev-occt/bitbybit-dev-occt';
-import { initializationComplete, onMessageInput } from '@bitbybit-dev/occt-worker';
+import createBitbybitOcct from "@bitbybit-dev/occt/bitbybit-dev-occt/cdn";
+import { type BitbybitOcctModule } from "@bitbybit-dev/occt/bitbybit-dev-occt/bitbybit-dev-occt";
+import { initializationComplete, onMessageInput } from "@bitbybit-dev/occt-worker";
 
 createBitbybitOcct().then((occ: BitbybitOcctModule) => {
     initializationComplete(occ, undefined);
 });
 
-addEventListener('message', ({ data }) => {
+addEventListener("message", ({ data }) => {
     onMessageInput(data, postMessage);
 });
