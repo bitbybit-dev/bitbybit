4341a7e4fae6b4774c71bd63f5c6f7421b7c7d13
Matas Ubarevicius <matas@bitbybit.dev>
2026-05-19 14:57:16 +0300

    update packages for laptop holder app

--- examples/react/babylonjs/laptop-holder/src/App.tsx
+++ examples/react/babylonjs/laptop-holder/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { BitByBitBase } from '@bitbybit-dev/babylonjs';
 import { OccStateEnum } from '@bitbybit-dev/occt-worker';
