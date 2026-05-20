ed35cc4c798f6a2e435a210813cd622b5572b095
Matas Ubarevicius <matas@bitbybit.dev>
2026-05-20 17:20:54 +0300

    update vuew

---
+++ examples/nuxt/babylonjs/basic/app.vue
@@ -0,0 +1,17 @@
+<script setup>
+import BitByBitComponent from "./BitByBitComponent.vue"
+</script>
+<template>
+    <ClientOnly fallback-tag="span" fallback="Loading bitbybit...">
+        <BitByBitComponent />
+    </ClientOnly>
+</template>
+
+<style>
+html, body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+</style>
+
