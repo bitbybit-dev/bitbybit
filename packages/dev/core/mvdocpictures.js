544c34c5bde5e3bf5ca0e9d6146b120521ef1ffc
Matas Ubarevicius <matas@bitbybit.dev>
2026-09-06 03:40:33 +0300

    Remove the legacy mvdocpictures script from core
    It moved Blockly documentation images with a package that no dependency
    ever declared, so it could not run, and it compiled into core's dist
    as an unrelated module.

--- packages/dev/core/mvdocpictures.js
+++
@@ -1,5 +0,0 @@
-const mvdir = require('mvdir');
-
-mvdir('blockly-images-zelos/', 'ts-api-docs/assets/images/blockly-images/', { copy: true }).then(err => {
-    if (!err) console.log('done.');
-});
