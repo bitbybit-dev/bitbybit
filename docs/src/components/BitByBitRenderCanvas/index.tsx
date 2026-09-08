cdd5e621d487faf6b92b638f7748a606dd80e252
Matas Ubarevicius <matas@bitbybit.dev>
2026-09-08 20:42:37 +0300

    v1.2.0-rc.0

--- docs/src/components/BitByBitRenderCanvas/index.tsx
+++ docs/src/components/BitByBitRenderCanvas/index.tsx
@@ -66,7 +66,7 @@ const BitByBitRenderCanvas: React.FC<Props> = React.memo(({
     const canvasContainerRef = useRef<HTMLDivElement>(null);
     const uniqueId = useId();
     const { siteConfig } = useDocusaurusContext();
-    const defaultVersion = siteConfig.customFields?.bitbybitVersion as string || "1.1.1";
+    const defaultVersion = siteConfig.customFields?.bitbybitVersion as string || "1.2.0-rc.0";
 
     const [isIframeReady, setIsIframeReady] = useState(false);
     const [loadIframe, setLoadIframe] = useState(!requireManualStart);
